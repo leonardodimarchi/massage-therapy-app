677a5c34702370913cb6ee87b0a046184204e141
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-10-06 18:36:02 -0300

    feature(pages/login): added login component

--- src/app/presenter/app-routing.module.ts
+++ src/app/presenter/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: '**', pathMatch: 'full', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/presenter/pages/login/login.module.ts
@@ -0,0 +1,16 @@
+import { LoginComponent } from './login.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginRoutingModule } from './login-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+  ],
+  declarations: [
+    LoginComponent,
+  ],
+})
+export class LoginModule { }
