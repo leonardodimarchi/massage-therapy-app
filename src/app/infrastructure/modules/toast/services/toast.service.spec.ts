2be9f391eeb73c55bc532aa6f6e42a316fc3fcf4
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-10-13 18:29:38 -0300

    test(infra/modules/toast): fixing test

--- src/app/infrastructure/modules/toast/services/toast.service.spec.ts
+++ src/app/infrastructure/modules/toast/services/toast.service.spec.ts
@@ -1,13 +1,15 @@
+import { ToastrService } from 'ngx-toastr';
 import { TestBed } from '@angular/core/testing';
 
 import { ToastService } from './toast.service';
 
 describe('ToastService', () => {
   let service: ToastService;
+  let toastr: jasmine.SpyObj<ToastrService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ToastService);
+    toastr = jasmine.createSpyObj('ToastrService', ['error'])
+    service = new ToastService(toastr);
   });
 
   it('should be created', () => {
