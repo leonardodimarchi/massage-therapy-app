44330fa4d5e9372cb899bee2ca1a9f5a4e0f4c09
User <leonardodimarchi@outlook.com>
2022-10-16 18:19:55 -0300

    feature(domain/usecases/user/login): saving user jwt at the storage

--- src/app/domain/usecases/user/login_usecase.spec.ts
+++ src/app/domain/usecases/user/login_usecase.spec.ts
@@ -1,21 +1,39 @@
+import { StorageServiceInterface } from 'src/app/infrastructure/modules/storage/models/storage-service-interface';
+import { mockedJwtProxy } from 'src/app/mocks/jwt/jwt_proxy.mock';
+import { storageKeys } from 'src/environments/storage_keys';
 import { UserRepositoryInterface } from './../../contracts/repositories/user_repository.interface';
 import { LoginUsecase } from "./login_usecase";
 
 describe('LoginUsecase', () => {
   let usecase: LoginUsecase;
   let repository: jasmine.SpyObj<UserRepositoryInterface>;
+  let storageServiceSpy: jasmine.SpyObj<StorageServiceInterface>;
 
   beforeEach(() => {
     repository = jasmine.createSpyObj('UserRepositoryInterface', ['login']);
-    usecase = new LoginUsecase(repository);
+    storageServiceSpy = jasmine.createSpyObj('StorageServiceInterface', ['set']);
+    usecase = new LoginUsecase(repository, storageServiceSpy);
   });
 
   it('should call login repository', async () => {
+    repository.login.and.resolveTo(mockedJwtProxy);
+
     await usecase.call({
       email: 'myemail@email.com',
       password: '123456'
     });
 
     expect(repository.login).toHaveBeenCalledTimes(1);
+  });
+
+  it('should set the jwt on the storage', async () => {
+    repository.login.and.resolveTo(mockedJwtProxy);
+
+    await usecase.call({
+      email: 'myemail@email.com',
+      password: '123456'
+    });
+
+    expect(storageServiceSpy.set).toHaveBeenCalledOnceWith(storageKeys.userToken, mockedJwtProxy.access_token);
   });
 });
--- src/app/domain/usecases/user/login_usecase.ts
+++ src/app/domain/usecases/user/login_usecase.ts
@@ -1,13 +1,18 @@
 import { UserRepositoryInterface } from './../../contracts/repositories/user_repository.interface';
 import { LoginPayload } from '../../contracts/payloads/user/login_payload';
 import { UseCase } from '../../shared/usecase';
+import { StorageServiceInterface } from 'src/app/infrastructure/modules/storage/models/storage-service-interface';
+import { storageKeys } from 'src/environments/storage_keys';
 
 export class LoginUsecase implements UseCase<LoginPayload, void> {
   constructor(
     private readonly repository: UserRepositoryInterface,
+    private readonly storageService: StorageServiceInterface,
   ) {}
 
   async call(params: LoginPayload): Promise<void> {
-    await this.repository.login(params);
+    const jwt = await this.repository.login(params);
+
+    await this.storageService.set(storageKeys.userToken, jwt.access_token);
   }
 }
