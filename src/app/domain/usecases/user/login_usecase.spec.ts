b320ce8bc9aee3723688fed164aa82bd16514b76
User <leonardodimarchi@outlook.com>
2022-10-17 21:29:00 -0300

    feature(usecases/login):  validating email at the login

--- src/app/domain/usecases/user/login_usecase.spec.ts
+++ src/app/domain/usecases/user/login_usecase.spec.ts
@@ -1,6 +1,8 @@
 import { StorageServiceInterface } from 'src/app/infrastructure/modules/storage/models/storage-service-interface';
 import { mockedJwtProxy } from 'src/app/mocks/jwt/jwt_proxy.mock';
 import { storageKeys } from 'src/environments/storage_keys';
+import { LoginPayload } from '../../contracts/payloads/user/login_payload';
+import { UserValidators } from '../../validators/user/user_validators';
 import { UserRepositoryInterface } from './../../contracts/repositories/user_repository.interface';
 import { LoginUsecase } from "./login_usecase";
 
@@ -13,11 +15,11 @@ describe('LoginUsecase', () => {
     repository = jasmine.createSpyObj('UserRepositoryInterface', ['login']);
     storageServiceSpy = jasmine.createSpyObj('StorageServiceInterface', ['set']);
     usecase = new LoginUsecase(repository, storageServiceSpy);
+
+    repository.login.and.resolveTo(mockedJwtProxy);
   });
 
   it('should call login repository', async () => {
-    repository.login.and.resolveTo(mockedJwtProxy);
-
     await usecase.call({
       email: 'myemail@email.com',
       password: '123456'
@@ -27,13 +29,38 @@ describe('LoginUsecase', () => {
   });
 
   it('should set the jwt on the storage', async () => {
-    repository.login.and.resolveTo(mockedJwtProxy);
-
     await usecase.call({
       email: 'myemail@email.com',
       password: '123456'
     });
 
     expect(storageServiceSpy.set).toHaveBeenCalledOnceWith(storageKeys.userToken, mockedJwtProxy.access_token);
+  });
+
+  describe('Validators', () => {
+    it('should call the email validator', async () => {
+      const validatorSpy = spyOn(UserValidators, 'email');
+      validatorSpy.and.returnValue(true);
+      
+      const payload: LoginPayload = {
+        email: 'myemail@email.com',
+        password: '123456'
+      };
+
+      await usecase.call(payload);
+
+      expect(validatorSpy).toHaveBeenCalledOnceWith(payload.email);
+    });
+
+    it('should throw and error if the email is invalid', async () => {
+      spyOn(UserValidators, 'email').and.returnValue(false);
+      
+      const payload: LoginPayload = {
+        email: 'myemail@email.com',
+        password: '123456'
+      };
+
+      await expectAsync(usecase.call(payload)).toBeRejectedWithError();
+    });
   });
 });
--- src/app/domain/usecases/user/login_usecase.ts
+++ src/app/domain/usecases/user/login_usecase.ts
@@ -3,14 +3,20 @@ import { LoginPayload } from '../../contracts/payloads/user/login_payload';
 import { UseCase } from '../../shared/usecase';
 import { StorageServiceInterface } from 'src/app/infrastructure/modules/storage/models/storage-service-interface';
 import { storageKeys } from 'src/environments/storage_keys';
+import { UserValidators } from '../../validators/user/user_validators';
 
 export class LoginUsecase implements UseCase<LoginPayload, void> {
   constructor(
     private readonly repository: UserRepositoryInterface,
     private readonly storageService: StorageServiceInterface,
-  ) {}
+  ) { }
 
   async call(params: LoginPayload): Promise<void> {
+    const isValidEmail = UserValidators.email(params.email);
+
+    if (!isValidEmail)
+      throw new Error('Por favor, enviar um e-mail válido.');
+
     const jwt = await this.repository.login(params);
 
     await this.storageService.set(storageKeys.userToken, jwt.access_token);
