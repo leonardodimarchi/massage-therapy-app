900640292035c0ce22a1ce1b147b3395f02a6346
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-10-27 21:50:39 -0300

    style(main): replacing single-quote with quotes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from '@presenter/app.module';
-import { environment } from '@env/environment';
+import { AppModule } from "@presenter/app.module";
+import { environment } from "@env/environment";
 
 if (environment.production) {
   enableProdMode();
