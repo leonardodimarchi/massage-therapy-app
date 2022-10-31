dfd7f9677859ee2fe93895373c6923c1216447f4
User <leonardodimarchi@outlook.com>
2022-10-31 18:54:55 -0300

    feature(pages/home): added home page component

---
+++ src/app/presenter/pages/home/home.component.spec.ts
@@ -0,0 +1,68 @@
+import { LoginUsecase } from "@domain/usecases/user/login_usecase";
+import { ToastService } from "@infra/modules/toast/services/toast.service";
+import { LoginComponent } from "@presenter/pages/login/login.component";
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+
+  let loginUsecase: jasmine.SpyObj<LoginUsecase>;
+  let toastService: jasmine.SpyObj<ToastService>;
+
+  beforeEach(async () => {
+    loginUsecase = jasmine.createSpyObj('LoginUsecase', ['call']);
+    toastService = jasmine.createSpyObj('ToastService', ['showError']);
+
+    component = new LoginComponent(loginUsecase, toastService);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('Login', () => {
+    it('should call the login usecase with the correct params', async () => {
+      const expectedEmail = 'mocked@email.com';
+      const expectedPassword = '123456';
+      component.form.controls.email.setValue(expectedEmail);
+      component.form.controls.password.setValue(expectedPassword);
+
+      await component.login();
+
+      expect(loginUsecase.call).toHaveBeenCalledOnceWith({
+        email: expectedEmail,
+        password: expectedPassword,
+      })
+    });
+
+    it('should show an error toast when receiving an error from the usecase', async () => {
+      const message = 'mocked error';
+      loginUsecase.call.and.throwError(message);
+
+      await component.login();
+
+      expect(toastService.showError).toHaveBeenCalledOnceWith({
+        message,
+      })
+    });
+
+    it('should set loading state as true when calling the usecase', () => {
+      component.login();
+
+      expect(component.isLoading).toBeTrue();
+    });
+
+    it('should set loading state as false when finishing the usecase call', async () => {
+      await component.login();
+
+      expect(component.isLoading).toBeFalse();
+    });
+
+    it('should not call usecase if it\'s already loading', async () => {
+      component.isLoading = true;
+
+      await component.login();
+
+      expect(loginUsecase.call).not.toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/app/presenter/pages/home/home.module.ts
@@ -0,0 +1,14 @@
+import { HomeComponent } from './home.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+    HomeComponent,
+  ],
+  providers: [],
+})
+export class HomeModule { }
