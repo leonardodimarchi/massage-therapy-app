4436e7f33eb712d694ffad8ab42ce1b275248c0b
User <leonardodimarchi@outlook.com>
2022-11-22 19:09:57 -0300

    feature(datasources/endpoints): added Appointment.getUserAppointments endpoint

--- src/app/infrastructure/datasources/endpoints.ts
+++ src/app/infrastructure/datasources/endpoints.ts
@@ -2,4 +2,8 @@ export class ApiEndpoints {
     static Auth = {
       login: () => '/auth/login',
     }
+
+    static Appointment = {
+      getUserAppointments: (page: number, limit: number) => `/appointments?page=${page}&limit=${limit}`,
+    }
 };
