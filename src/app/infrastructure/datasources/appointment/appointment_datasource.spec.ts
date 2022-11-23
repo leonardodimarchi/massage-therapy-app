29457b265f7be7320d11a9ac722d0532d7773c73
User <leonardodimarchi@outlook.com>
2022-11-23 19:46:22 -0300

    feature(datasources/appoinappointment): started to implement getUserAppointments

--- src/app/infrastructure/datasources/appointment/appointment_datasource.spec.ts
+++ src/app/infrastructure/datasources/appointment/appointment_datasource.spec.ts
@@ -1,14 +1,33 @@
+import { ApiEndpoints } from './../endpoints';
 import { AppointmentDatasource } from './appointment_datasource';
 import { AppointmentDatasourceInterface } from "@infra/contracts/datasources";
+import { HttpServiceInterface } from '@domain/contracts/services';
 
 describe('AppointmentDatasource', () => {
     let datasource: AppointmentDatasourceInterface;
+    let httpService: jasmine.SpyObj<HttpServiceInterface>;
 
     beforeEach(() => {
-      datasource = new AppointmentDatasource();
+      httpService = jasmine.createSpyObj('HttpServiceInterface', ['get']);
+      datasource = new AppointmentDatasource(httpService);
     });
 
     it('should initialize', () => {
       expect(datasource).toBeDefined();
     });
+
+    describe('GetUserAppointments', () => {
+      it('should call HTTP GET with the correct url and payload', async () => {
+        const page = 1;
+        const limit = 5;
+        const url = ApiEndpoints.Appointment.getUserAppointments(page, limit);
+
+        await datasource.getUserAppointments({
+          page,
+          limit,
+        });
+
+        expect(httpService.get).toHaveBeenCalledOnceWith(url);
+      });
+    })
 });
--- src/app/infrastructure/datasources/appointment/appointment_datasource.ts
+++ src/app/infrastructure/datasources/appointment/appointment_datasource.ts
@@ -1,10 +1,27 @@
+import { ApiEndpoints } from './../endpoints';
 import { GetUserAppointmentsParams } from '@domain/contracts/repositories';
+import { HttpServiceInterface } from '@domain/contracts/services';
 import { AppointmentEntity } from '@domain/entities/appointment/appointment_entity';
 import { PaginatedItems } from '@domain/models/interfaces/paginated-items.interface';
 import { AppointmentDatasourceInterface } from '@infra/contracts/datasources';
 
 export class AppointmentDatasource implements AppointmentDatasourceInterface {
-  getUserAppointments(params: GetUserAppointmentsParams): Promise<PaginatedItems<AppointmentEntity>> {
-    throw new Error('Method not implemented.');
+
+  constructor(
+    private readonly httpService: HttpServiceInterface,
+  ) { }
+
+  async getUserAppointments({ page, limit }: GetUserAppointmentsParams): Promise<PaginatedItems<AppointmentEntity>> {
+    const url = ApiEndpoints.Appointment.getUserAppointments(page, limit);
+
+    await this.httpService.get(url);
+
+    return {
+      count: 1,
+      items: [],
+      page: 1,
+      pageCount: 1,
+      total: 1,
+    };
   }
 }
