fbaabf205c31c5f3141b60efed584f7ed6079d1d
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-12-17 11:43:24 -0300

    feature(android): adding capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dimarchi.massagetherapy',
+  appName: 'Dimarchi Massoterapia',
+  webDir: 'dist/massage-therapy-app',
+  bundledWebRuntime: false
+};
+
+export default config;
