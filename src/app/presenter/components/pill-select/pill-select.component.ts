a8074be01e4029988ade4d120e930edc3d0c5165
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-02-15 17:52:20 -0300

    feature(components/pill-select): adding PillSelectComponent files

---
+++ src/app/presenter/components/pill-select/pill-select.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pill-select',
+  templateUrl: './pill-select.component.html',
+  styleUrls: ['./pill-select.component.scss'],
+})
+export class PillSelectComponent {}
