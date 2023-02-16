ac9828f1123bf4373cac9fcc5fc5ccabcf7236ae
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-02-16 17:20:28 -0300

    feature(components/pill-select): added PillSelectItem interface

--- src/app/presenter/components/pill-select/pill-select.component.ts
+++ src/app/presenter/components/pill-select/pill-select.component.ts
@@ -1,8 +1,19 @@
 import { Component, Input } from '@angular/core';
 
+interface PillSelectItem<TValue = any> {
+  label: string;
+  value: TValue;
+}
+
 @Component({
   selector: 'app-pill-select',
   templateUrl: './pill-select.component.html',
   styleUrls: ['./pill-select.component.scss'],
 })
-export class PillSelectComponent {}
+export class PillSelectComponent {
+  @Input()
+  public items: PillSelectItem[] = [];
+
+  @Input()
+  public value: any;
+}
