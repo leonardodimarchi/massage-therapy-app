f3c445315fbe7b1cc5467c8fb1c8e4cfcac17490
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-03-23 17:13:29 -0300

    feature(pages/register): adding mask at the address postalCode

--- src/app/presenter/pages/register/components/address-information-form/address-information-form.module.ts
+++ src/app/presenter/pages/register/components/address-information-form/address-information-form.module.ts
@@ -1,3 +1,4 @@
+import { NgxMaskModule } from 'ngx-mask';
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
@@ -9,6 +10,7 @@ import { AddressInformationFormComponent } from "./address-information-form.comp
     CommonModule,
     ReactiveFormsModule,
     LoadingSpinnerModule,
+    NgxMaskModule.forChild(),
   ],
   declarations: [
     AddressInformationFormComponent,
