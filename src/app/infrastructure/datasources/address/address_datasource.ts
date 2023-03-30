3b1991638027ce371cf75e1ca078fcb0b5f79ab4
User <leonardodimarchi@outlook.com>
2023-03-30 19:47:15 -0300

    feature(datasources/address): adding ceppromise

--- src/app/infrastructure/datasources/address/address_datasource.ts
+++ src/app/infrastructure/datasources/address/address_datasource.ts
@@ -1,10 +1,11 @@
 import { AddressDatasourceInterface } from "@infra/contracts/datasources";
 import { AddressByPostalCodeDto } from "@infra/models/address/dto/address_by_postal_code_dto";
+import { cep } from "cep-promise";
 
 export class PostalCodeDatasource implements AddressDatasourceInterface {
 
   async getAddressByPostalCode(postalCode: string): Promise<AddressByPostalCodeDto> {
-    throw new Error("Method not implemented.");
+    return await cep(postalCode);
   }
   
 }
